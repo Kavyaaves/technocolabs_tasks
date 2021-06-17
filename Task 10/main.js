1017f45b4e4a0eac6baa862c64ff23f91af2c123
Kavya Mm <kavyaaves2019@gmail.com>
2021-06-17 14:58:55 +0530

    Task 10 completed

---
+++ Task 10/main.js
@@ -0,0 +1,51 @@
+var userInput = 'Paper';
+userInput = userInput.toLowerCase();
+
+function getComputerChoice() {
+	switch (Math.floor(Math.random() * 3)) {
+		case 0:
+			return 'rock';
+		case 1:
+			return 'paper';
+		case 2:
+			return 'scissors';
+	}
+}
+
+function determineWinner() {
+	if (userChoice === computerChoice) {
+		return 'Game is a tie!';
+	}
+	if (userChoice === 'rock') {
+		if (computerChoice === 'paper') {
+			return 'Computer wins!';
+		} else {
+			return 'User wins!';
+		}
+	}
+	if (userChoice === 'paper') {
+		if (computerChoice === 'scissors') {
+			return 'Computer wins!';
+		} else {
+			return 'User wins!';
+		}
+	}
+	if (userChoice === 'scissors') {
+		if (computerChoice === 'rock') {
+			return 'Computer wins!';
+		} else {
+			return 'User wins!';
+		}
+	}
+}
+
+var userChoice = userInput;
+var computerChoice = getComputerChoice();
+
+function playGame() {
+	console.log('You threw: ' + userChoice);
+	console.log('Computer threw: ' + computerChoice);
+	console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
